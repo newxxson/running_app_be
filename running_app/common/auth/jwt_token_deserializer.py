d50791b899ec2d0cbd507d5b605579f4f2785c11
newxxson <hyukjun1111@gmail.com>
2024-11-09 16:57:46 +0900

    feature/#1: 유저 보안 로직 추가

---
+++ running_app/common/auth/jwt_token_deserializer.py
@@ -0,0 +1,32 @@
+# Define a user model
+from fastapi import Depends, HTTPException, Header
+import jwt
+from pydantic import BaseModel
+from running_app.common.property.auth_property import auth_property
+
+
+class TokenData(BaseModel):
+    identifier: str | None = None
+
+
+async def get_current_user(authorization: str | None = Header(None)):
+    if authorization is None or not authorization.startswith("Bearer "):
+        raise HTTPException(
+            status_code=401, detail="Authorization header missing or malformed"
+        )
+
+    token = authorization.split(" ")[1]  # Extract the token part after "Bearer"
+
+    try:
+        # Decode the token
+        payload = jwt.decode(
+            token, auth_property.jwt_secret_key, algorithms=[auth_property.algorithm]
+        )
+        user_identifier: str | None = payload.get("sub")
+        if user_identifier is None:
+            raise HTTPException(
+                status_code=401, detail="User identifier missing in token"
+            )
+        return {"user_identifier": user_identifier}
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
--- running_app/user/adapter/input/web/user_controller.py
+++ running_app/user/adapter/input/web/user_controller.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 from uuid import UUID
-from fastapi import APIRouter, Depends, File, Request, UploadFile
+from fastapi import APIRouter, Depends, File, HTTPException, Request, UploadFile
 
+from running_app.common.auth.jwt_token_deserializer import get_current_user
 from running_app.common.di import on
 from running_app.user.adapter.input.web.request.create_user_request import (
     CreateUserRequest,
@@ -12,6 +13,7 @@ from running_app.user.application.port.input.create_user_usecase import (
     CreateUserUseCase,
 )
 from running_app.user.application.port.input.login_user_usecase import LoginUserUseCase
+from running_app.user.application.port.input.query_user_usecase import QueryUserUseCase
 from running_app.user.domain.model.auth_token import AuthPayload
 
 
@@ -39,3 +41,17 @@ async def login(
     return await login_user_usecase.login_user(
         kakao_token=login_request.kakao_auth_token
     )
+
+
+@user_router.get("users/{user_identifier}")
+async def get_user(
+    query_user_usecase: Annotated[QueryUserUseCase, Depends(on(QueryUserUseCase))],
+    user_identifier: UUID,
+    current_user_identifier: UUID = Depends(get_current_user),
+) -> UserResponse:
+    """Get user."""
+    if user_identifier != current_user_identifier:
+        raise HTTPException(status_code=403, detail="Forbidden")
+
+    user = await query_user_usecase.find_user_by_id(user_identifier=user_identifier)
+    return UserResponse.from_domain(user)
