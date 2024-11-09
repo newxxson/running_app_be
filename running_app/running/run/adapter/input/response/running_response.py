4333e0b4ff7397277ca85cafe1c0900d1dae8d6d
newxxson <hyukjun1111@gmail.com>
2024-11-10 04:48:53 +0900

    fix: running response

--- running_app/running/run/adapter/input/response/running_response.py
+++ running_app/running/run/adapter/input/response/running_response.py
@@ -21,9 +21,9 @@ class RunningResponse(BaseModel):
 
     running_user_identifiers: list[UUID]
 
-    total_distance: float
+    total_distance: float | None
 
-    path_identifier: UUID
+    path_identifier: UUID | None
 
     created_date: datetime.datetime
 
